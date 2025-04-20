575637fd2e02c02f0bb3645dde3d53c77816330d
Andreas Doehler <andreas.doehler@gmail.com>
2025-04-20 18:05:18 +0000

    fix for container user root

--- .devcontainer/setpwd.sh
+++ .devcontainer/setpwd.sh
@@ -7,3 +7,4 @@ rm -rfv $OMD_ROOT/local/tmp
 ln -sv $WORKSPACE/temp $OMD_ROOT/local/tmp
 
 source /omd/sites/cmk/.profile && echo 'cmkadmin' | /omd/sites/cmk/bin/cmk-passwd -i cmkadmin
+chown cmk:cmk /omd/sites/cmk/etc/htpasswd
--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -3,3 +3,5 @@
 find $OMD_ROOT/tmp/ -name "*.pid" -exec rm {} \;
 
 source /omd/sites/cmk/.profile && $OMD_ROOT/bin/omd restart
+
+omd su cmk
