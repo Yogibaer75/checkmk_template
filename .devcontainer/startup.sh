73b14418852e91b3bb9368f2e2d9232a3bf17948
Andreas Doehler <andreas.doehler@gmail.com>
2025-07-12 16:45:05 +0000

    p7 update startup modification

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -2,8 +2,4 @@
 
 find $OMD_ROOT/tmp/ -name "*.pid" -exec rm {} \;
 
-source /omd/sites/cmk/.profile && $OMD_ROOT/bin/omd restart
-
-omd su cmk
-
-/omd/sites/cmk/bin/cmk-update-license-usage
+source /omd/sites/cmk/.profile && $OMD_ROOT/bin/omd restart && $OMD_ROOT/bin/omd su cmk && $OMD_ROOT/bin/cmk-update-license-usage
