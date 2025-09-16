f013ce71e2e54dcf15b9f5a7085d7788fb3c88c5
Andreas Doehler <andreas.doehler@gmail.com>
2025-09-16 08:18:46 +0000

    p11 update

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 find $OMD_ROOT/tmp/ -name "*.pid" -exec rm {} \;
-
-source /omd/sites/cmk/.profile && $OMD_ROOT/bin/omd restart && $OMD_ROOT/bin/omd su cmk && $OMD_ROOT/bin/cmk-update-license-usage
